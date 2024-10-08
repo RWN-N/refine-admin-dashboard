449dc6a994e734aa132d0d8ceb6eb8606a9e5abf
RWN-N <rowenbos@gmail.com>
2024-10-09 00:07:46 +0700

    fix force ts

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -6,12 +6,15 @@ import KanbanColumn from '@/components/tasks/kanban/column'
 import KanbanItem from '@/components/tasks/kanban/item'
 import { UPDATE_TASK_STAGE_MUTATION } from '@/graphql/mutations'
 import { TASK_STAGES_QUERY, TASKS_QUERY } from '@/graphql/queries'
-import { TaskStage } from '@/graphql/schema.types'
-import { TasksQuery } from '@/graphql/types'
+import { TasksQuery, TaskStagesQuery } from '@/graphql/types'
 import { DragEndEvent } from '@dnd-kit/core'
 import { useList, useNavigation, useUpdate } from '@refinedev/core'
 import { GetFieldsFromList } from '@refinedev/nestjs-query'
 import React from 'react'
+
+
+type Task = GetFieldsFromList<TasksQuery>;
+type TaskStage = GetFieldsFromList<TaskStagesQuery> & { tasks: Task[] }
 
 const TaskList = ({ children }: React.PropsWithChildren) => {
     const { replace } = useNavigation();
@@ -65,7 +68,7 @@ const TaskList = ({ children }: React.PropsWithChildren) => {
             }
         }
         const unassignedStage = tasks.data.filter((task) => task.stageId === null)
-        const groupedStage = stages.data.map((stage) => ({
+        const groupedStage: TaskStage[] = stages.data.map((stage) => ({
             ...stage, 
             tasks: tasks.data.filter((task) => task?.stageId?.toString() === stage.id)
         }))
