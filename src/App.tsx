fa0bfbe0ae4b92001d1a81c95e60272a065c14ba
RWN-N <rowenbos@gmail.com>
2024-10-08 23:56:25 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,99 @@
+import { Authenticated, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import {
+  ErrorComponent,
+  ThemedLayoutV2,
+  ThemedSiderV2,
+  useNotificationProvider,
+} from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import { authProvider, dataProvider, liveProvider } from "./providers";
+import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
+import { Layout } from "./components/layout";
+
+import routerBindings, {
+  CatchAllNavigate,
+  DocumentTitleHandler,
+  NavigateToResource,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { App as AntdApp } from "antd";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { resources } from "./config/resources";
+import CreateCompany from "./pages/company/create";
+import EditCompany from "./pages/company/edit";
+import TaskList from "./pages/tasks/list";
+import TaskCreate from "./pages/tasks/create";
+import TaskEdit from "./pages/tasks/edit";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RefineKbarProvider>
+        <AntdApp>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              liveProvider={liveProvider}
+              notificationProvider={useNotificationProvider}
+              routerProvider={routerBindings}
+              authProvider={authProvider}
+              resources={resources}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                useNewQueryKeys: true,
+                projectId: "QBzH0l-GrS9KW-G66grW",
+                liveMode: "auto",
+              }}
+            >
+
+              <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route
+                  element={
+                    <Authenticated
+                      key="authenticated-layout"
+                      fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }
+                >
+                  <Route index element={<Home />} />
+                  <Route path="/companies">
+                    <Route index element={<CompanyList />} />
+                    <Route path="new" element={<CreateCompany />} />
+                    <Route path="edit/:id" element={<EditCompany />} />
+                  </Route>
+                  <Route path="/tasks" element={
+                    <TaskList>
+                      <Outlet />
+                    </TaskList>
+                  }>
+                    <Route path="new" element={<TaskCreate />}/>
+                    <Route path="edit/:id" element={<TaskEdit />}/>
+                  </Route>
+                </Route>
+              </Routes>
+
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </AntdApp>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,27 @@
+import UpcomingEvents from './home/upcoming-events'
+import DealsChart from './home/deals-chart'
+import UpcomingEventsSkeleton from './skeleton/upcoming-events'
+import AccordionHeaderSkeleton from './skeleton/accordion-header'
+import KanbanColumnSkeleton from './skeleton/kanban'
+import ProjectCardSkeleton from './skeleton/project-card'
+import DashboardTotalCountCard from './home/total-count-card'
+import DashboardLatestActivities from './home/latest-activities'
+
+export { 
+    UpcomingEvents, 
+    DealsChart, 
+    UpcomingEventsSkeleton, 
+    AccordionHeaderSkeleton, 
+    KanbanColumnSkeleton, 
+    ProjectCardSkeleton,
+    DashboardTotalCountCard,
+    DashboardLatestActivities,
+};
+
+export * from "./accordion";
+export * from "./tasks/form/description";
+export * from "./tasks/form/due-date";
+export * from "./tasks/form/stage";
+export * from "./tasks/form/title";
+export * from "./tasks/form/users";
+export * from "./tasks/form/header";
